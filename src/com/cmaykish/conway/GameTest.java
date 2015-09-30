b503afa6bada32edef8a9a2660bf433c4d4f2f76
Colin Maykish <crmaykish@gmail.com>
2015-09-30 10:18:52 -0400

    added more unit tests

--- src/com/cmaykish/conway/GameTest.java
+++ src/com/cmaykish/conway/GameTest.java
@@ -5,28 +5,118 @@ import org.junit.Test;
 
 public class GameTest {
 
-    private int[][] startingBoard = { // Sample board given in assignment
-            {0,0,0,0,0,0,1,0},
-            {1,1,1,0,0,0,1,0},
-            {0,0,0,0,0,0,1,0},
-            {0,0,0,0,0,0,0,0},
-            {0,0,0,1,1,0,0,0},
-            {0,0,0,1,1,0,0,0}
-    };
-
-    private int[][] endingBoard = { // Sample board given in assignment
-            {0,1,0,0,0,0,0,0},
-            {0,1,0,0,0,1,1,1},
-            {0,1,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0},
-            {0,0,0,1,1,0,0,0},
-            {0,0,0,1,1,0,0,0}
-    };
-
+    /**
+     * Two living cells die from starvation
+     */
     @Test
-    public void testOneStep() throws Exception {
-        Game game = new Game(startingBoard);
+    public void testStarvationRule(){
+        int[][] start = {
+                {1,0},
+                {0,1}
+        };
+
+        int[][] end = {
+                {0,0},
+                {0,0}
+        };
+
+        Game game = new Game(start);
         int[][] result = game.step();
-        Assert.assertArrayEquals(endingBoard, result);
+        Assert.assertArrayEquals(end, result);
+    }
+
+    /**
+     * Center cell dies from overcrowding
+     */
+    @Test
+    public void testOvercrowdingRule(){
+        int[][] start = {
+                {1,1,0},
+                {0,1,0},
+                {1,0,1}
+        };
+
+        int[][] end = {
+                {1,1,0},
+                {0,0,1},
+                {0,1,0}
+        };
+
+        Game game = new Game(start);
+        int[][] result = game.step();
+        Assert.assertArrayEquals(end, result);
+    }
+
+    /**
+     * Center cell comes to life for having 3 neighbors
+     */
+    @Test
+    public void testNewLifeRule(){
+        int[][] start = {
+                {1,0,1},
+                {0,0,0},
+                {1,0,0}
+        };
+
+        int[][] end = {
+                {0,0,0},
+                {0,1,0},
+                {0,0,0}
+        };
+
+        Game game = new Game(start);
+        int[][] result = game.step();
+        Assert.assertArrayEquals(end, result);
+    }
+
+    /**
+     * All living cells have exactly three neighbors and no dead cells have three so board state remains the same
+     */
+    @Test
+    public void testMaintainStateRule(){
+        int[][] start = {
+                {1,1,0},
+                {1,1,0},
+                {0,0,0}
+        };
+
+        int[][] end = {
+                {1,1,0},
+                {1,1,0},
+                {0,0,0}
+        };
+
+        Game game = new Game(start);
+        int[][] result = game.step();
+        Assert.assertArrayEquals(end, result);
+    }
+
+    /**
+     * Complete board test - test all rules in one step
+     */
+    @Test
+    public void testFullStep(){
+
+        int[][] start = { // Sample starting board given in assignment
+                {0,0,0,0,0,0,1,0},
+                {1,1,1,0,0,0,1,0},
+                {0,0,0,0,0,0,1,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,1,1,0,0,0},
+                {0,0,0,1,1,0,0,0}
+        };
+
+        int[][] end = { // Expected result after one step of the game
+                {0,1,0,0,0,0,0,0},
+                {0,1,0,0,0,1,1,1},
+                {0,1,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,1,1,0,0,0},
+                {0,0,0,1,1,0,0,0}
+        };
+
+        Game game = new Game(start);
+        int[][] result = game.step();
+        Assert.assertArrayEquals(end, result);
     }
 }
